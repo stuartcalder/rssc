7f5a210ddaf50ed9bcea72b4b8c1e623b8593172
Stuart Calder <technoglub@protonmail.com>
2024-10-12 14:19:16 -0700

    Seems to have successfully linked with libSSC.so

--- src/c.rs
+++ src/c.rs
@@ -17,3 +17,22 @@ pub mod file {
     pub const IS_PTR: bool = true;
     pub const NULL:  CType = -1 as c_type;
 }
+
+#[link(name = "SSC")]
+extern "C" {
+    pub fn SSC_File_getSize(file: file::CType, storesize: *mut cty::size_t) -> ErrorT;
+    pub fn SSC_FilePath_getSize(fpath: *const cty::c_char, storesize: *mut cty::size_t) -> ErrorT;
+    pub fn SSC_FilePath_exists(fpath: *const cty::c_char) -> bool;
+    pub fn SSC_FilePath_forceExistOrDie(fpath: *const cty::c_char, control: bool) -> ();
+    pub fn SSC_FilePath_open(
+        fpath: *const cty::c_char,
+        readonly: bool, 
+        storefile: *mut file::CType) -> ErrorT;
+    pub fn SSC_FilePath_create(
+        fpath: *const cty::c_char,
+        storefile: *mut file::CType) -> ErrorT;
+    /* TODO: Implement SSC_File_createSecret() later? */
+    pub fn SSC_File_close(file: file::CType) -> ErrorT;
+    pub fn SSC_File_setSize(file: file::CType, size: cty::size_t) -> ErrorT;
+    pub fn SSC_chdir(fpath: *const cty::c_char) -> ErrorT;
+}
