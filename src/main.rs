6f50a97a8e0ef9dee0dd32cb44a888a5a730609d
Stuart Calder <technoglub@protonmail.com>
2024-10-13 11:06:33 -0700

    Add the SSC_MemMap_* procedures.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
+
 fn main() {
-    if ssc::c::file::IS_INT {
+    use std::ffi::CString;
+    use ssc::c;
+    use c::file;
+
+    if file::IS_INT {
         println!("In this implementation, the files are integers.");
     } else {
         println!("In this implementation, the files are probably pointers.");
+    }
+
+    let mut c_file: file::Type = file::NULL;
+    println!("Prior to calling SSC_FilePath_create(), c_file was {c_file}");
+    let path = CString::new("/ram/u/test.txt").unwrap();
+    let err = unsafe {
+        c::SSC_FilePath_create(path.as_ptr() as *const cty::c_char, &mut c_file as *mut file::Type)
+    };
+    println!("The c_file became {c_file}");
+    println!("The path became {:?}", path);
+    println!("The err beccame {err}");
+
+    {
+        let nproc = unsafe { c::SSC_getNumberProcessors() };
+        println!("We have {nproc} processors.");
     }
 }
