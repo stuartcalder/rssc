3a158a3e9e0eb18ec6f9e91fab1eca4391638d5c
Stuart Calder <technoglub@protonmail.com>
2024-10-14 00:14:14 -0700

    Continue implementing and experimenting with Rust.

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,43 @@ fn main() {
     use ssc::c;
     use ssc::mmap;
     use mmap::file;
+    use mmap::init_flag;
+    use mmap::init_code;
 
     if file::IS_INT {
         println!("In this implementation, the files are integers.");
     } else {
         println!("In this implementation, the files are probably pointers.");
     }
-
-    let mut c_file: file::Type = file::NULL;
-    println!("Prior to calling SSC_FilePath_create(), c_file was {c_file}");
-    let path = CString::new("/ram/u/test.txt").unwrap();
-    let err = unsafe {
-        mmap::SSC_FilePath_create(path.as_ptr() as *const cty::c_char, &mut c_file as *mut file::Type)
-    };
-    println!("The c_file became {c_file}");
-    println!("The path became {:?}", path);
-    println!("The err beccame {err}");
-
-    {
-        let nproc = unsafe { c::SSC_getNumberProcessors() };
-        println!("We have {nproc} processors.");
+    
+    let fpath = CString::new("/ram/u/mmap_test").unwrap();
+    let size  = 1024usize * 1024usize;
+    let map_result = mmap::Map::new(
+        &fpath, 
+        size,
+        init_flag::ALLOW_SHRINK |
+        init_flag::FORCE_EXIST
+    );
+    match map_result {
+        Ok(mut m) => {
+            m.get_slice().fill(0xffu8);
+            println!("Filled the memory map with 0xff!");
+        },
+        Err(e) => {
+            let msg:&'static str = match e {
+                init_code::ERR_FILE_EXIST_NO   => "Failed to force a file to not exist!",
+                init_code::ERR_FILE_EXIST_YES  => "Failed to force a file to exist!",
+                init_code::ERR_READONLY        => "Violated readonly!",
+                init_code::ERR_SHRINK          => "Attempted to shrink while disallowed!",
+                init_code::ERR_NO_SIZE         => "Size not provided!",
+                init_code::ERR_OPEN_FILEPATH   => "Failed to open the filepath!",
+                init_code::ERR_CREATE_FILEPATH => "Failed to create the filepath!",
+                init_code::ERR_GET_FILE_SIZE   => "Failed to get the file size!",
+                init_code::ERR_SET_FILE_SIZE   => "Failed to set the file size!",
+                init_code::ERR_MAP             => "Failed to map the file!",
+                _                              => "Unaccounted for error!"
+            };
+            eprintln!("{msg}");
+        },
     }
 }
--- src/mmap.rs
+++ src/mmap.rs
@@ -1,4 +1,5 @@
 
+
 use crate::c;
 
 pub mod file {
@@ -16,73 +17,127 @@ pub mod file {
     pub const NULL: Type = -1isize as Type;
 }
 
-pub mod mmap {
-    enum Init {
-        ReadOnly      = 0x01,
-        AllowShrink   = 0x02,
-        ForceExist    = 0x04,
-        ForceExistYes = 0x08,
-    }
-    enum InitCode {
-        Okay              =  0,
-        ErrFileExistNo    = -1, /* Failure to force non-existence of a file. */
-        ErrFileExistYes   = -2, /* Failure to force existence of a file. */
-        ErrReadOnly       = -3, /* Failure to enforce read-only. */
-        ErrShrink         = -4, /* Attempted to shrink while disallowed */
-        ErrNoSize         = -5, /* Size not provided. */
-        ErrOpenFilePath   = -6, /* Failed to open a filepath. */
-        ErrCreateFilePath = -7, /* Failed to create a file at a filepath. */
-        ErrGetFileSize    = -8, /* Failed to get a file size. */
-        ErrSetFileSize    = -9, /* Failed to set a file size. */
-        ErrMap            = -10, /* Failed to map a file into memory. */
-    }
-
-    #[repr(C)]
-    pub struct Map {
-        ptr: *mut cty::uint8_t,
-        size: cty::size_t,
-        file: super::file::Type,
-        #[cfg(target_family = "windows")]
-        windows_filemap: super::file::Type,
-        readonly: bool,
-    }
-
-    impl Map {
-        pub fn new() -> Map {
-            Map {
-                ptr: std::ptr::null_mut::<cty::uint8_t>(),
-                size: 0usize,
-                file: super::file::NULL,
-                #[cfg(target_family = "windows")]
-                windows_filemap: super::file::NULL,
-                readonly: false,
-            }
-        }
-        pub fn nullify(&mut self) {
-            if self.ptr != std::ptr::null_mut() {
-                unsafe { super::SSC_MemMap_del(self) };
-            }
-            self.ptr = std::ptr::null_mut::<cty::uint8_t>();
-            self.size = 0usize;
-            self.file = super::file::NULL;
-            #[cfg(target_family = "windows")]
-            {
-                self.windows_filemap = super::file::NULL;
-            }
-            self.readonly = false;
-        }
-        pub fn as_ptr(&mut self) -> *mut Map {
-            self as *mut Map
-        }
-    } // ~ impl Map
-    impl Drop for Map {
-        fn drop(&mut self) {
-            if self.ptr != std::ptr::null_mut() {
-                unsafe { super::SSC_MemMap_del(self) };
-            }
-        }
-    } // ~ impl Drop for Map
+pub mod init_flag {
+    use crate::c;
+    pub const READ_ONLY:       c::BitFlag = 0x01; /* Disallow writing to memory-map. */
+    pub const ALLOW_SHRINK:    c::BitFlag = 0x02; /* Allow shrinking the size of the mapped memory. */
+    pub const FORCE_EXIST:     c::BitFlag = 0x04; /* Force a file to NOT exist, unless ForceExistYes is on... */
+    pub const FORCE_EXIST_YES: c::BitFlag = 0x08; /* Force a file to exist, when @ForceExist is also on. */
 }
+pub mod init_code {
+    use crate::c;
+    pub const OK:                  c::CodeError =   0;
+    pub const ERR_FILE_EXIST_NO:   c::CodeError =  -1; /* Failure to force non-existence of a file. */
+    pub const ERR_FILE_EXIST_YES:  c::CodeError =  -2; /* Failure to force existence of a file. */
+    pub const ERR_READONLY:        c::CodeError =  -3; /* Failure to enforce read-only. */
+    pub const ERR_SHRINK:          c::CodeError =  -4; /* Attempted to shrink while disallowed */
+    pub const ERR_NO_SIZE:         c::CodeError =  -5; /* Size not provided. */
+    pub const ERR_OPEN_FILEPATH:   c::CodeError =  -6; /* Failed to open a filepath. */
+    pub const ERR_CREATE_FILEPATH: c::CodeError =  -7; /* Failed to create a file at a filepath. */
+    pub const ERR_GET_FILE_SIZE:   c::CodeError =  -8; /* Failed to get a file size. */
+    pub const ERR_SET_FILE_SIZE:   c::CodeError =  -9; /* Failed to set a file size. */
+    pub const ERR_MAP:             c::CodeError = -10; /* Failed to map a file into memory. */
+}
+
+#[repr(C)]
+pub struct Map {
+    ptr: *mut cty::uint8_t,
+    size: cty::size_t,
+    file: file::Type,
+    #[cfg(target_family = "windows")]
+    windows_filemap: file::Type,
+    readonly: bool,
+}
+
+use std::ffi::CString;
+impl Map {
+    /// Return a default-initialized Memory Map.
+    pub fn new_null() -> Self {
+        Self {
+            ptr: std::ptr::null_mut::<cty::uint8_t>(),
+            size: 0usize,
+            file: file::NULL,
+            #[cfg(target_family = "windows")]
+            windows_filemap: file::NULL,
+            readonly: false,
+        }
+    }
+
+    /// Initialize an existing Memory Map. Nullify if it's already initialized.
+    pub fn init(
+        self:     &mut Self,
+        filepath: &CString,
+        size:     cty::size_t,
+        flags:    c::BitFlag
+    ) -> Result<(), c::CodeError>
+    {
+        if self.ptr != std::ptr::null_mut() {
+            self.nullify();
+        }
+        let code = unsafe {
+            SSC_MemMap_init(
+                self as *mut Self,
+                filepath.as_ptr(),
+                size,
+                flags
+            )
+        };
+        match code {
+            init_code::OK => Ok(()),
+            _             => Err(code)
+        }
+    }
+
+    /// Return an initialized, mapped Memory Map.
+    pub fn new(
+        filepath: &CString,
+        size:     cty::size_t,
+        flags:    c::BitFlag
+    ) -> Result<Self, c::CodeError>
+    {
+        let mut m = Self::new_null();
+        m.init(filepath, size, flags)?;
+        Ok(m)
+    }
+
+    /// Free Memory map's resources and nullify variables.
+    pub fn nullify(&mut self) {
+        if self.ptr != std::ptr::null_mut() {
+            unsafe { SSC_MemMap_del(self as *mut Self) };
+        }
+    }
+
+    /// Return the (possibly) mapped memory as a mutable u8 pointer.
+    pub fn get_ptr(&mut self) -> *mut cty::uint8_t {
+        self.ptr
+    }
+
+    /// Return the size of the (possibly) mapped file, or 0 if no file has been mapped.
+    pub fn get_size(&self) -> cty::size_t {
+        self.size
+    }
+
+    /// Return whether we are allowed to write to the mapped memory of the Memory Map.
+    pub fn get_readonly(&self) -> bool {
+        self.readonly
+    }
+
+    /// Return a u8 slice representing the memory-mapped data.
+    pub fn get_slice(&mut self) -> &mut [cty::uint8_t] {
+        if self.ptr == std::ptr::null_mut() {
+            &mut []
+        } else {
+            unsafe { std::slice::from_raw_parts_mut(self.get_ptr(), self.get_size()) }
+        }
+    }
+
+} // ~ impl Map
+impl Drop for Map {
+    /// When Dropped, check to see if the Map has been memory- mapped. If so call SSC_MemMap_del().
+    fn drop(&mut self) {
+        self.nullify();
+    }
+} // ~ impl Drop for Map
 
 #[link(name = "SSC")]
 extern "C" {
@@ -105,19 +160,20 @@ extern "C" {
     pub fn SSC_chdir(fpath: *const cty::c_char) -> c::Error;
 /* MemMap procedures */
     pub fn SSC_MemMap_init(
-        map:      *mut mmap::Map,
+        map:      *mut Map,
         filepath: *const cty::c_char,
         size:     cty::size_t,
         flags:    c::BitFlag) -> c::CodeError;
+    #[cfg(feature = "Disable")]
     pub fn SSC_MemMap_initOrDie(
-        map:      *mut mmap::Map,
+        map:      *mut Map,
         filepath: *const cty::c_char,
         size:     cty::size_t,
         flags:    c::BitFlag) -> ();
     pub fn SSC_MemMap_map(
-        map: *mut mmap::Map,
+        map: *mut Map,
         readonly: bool) -> c::Error;
-    pub fn SSC_MemMap_unmap(map: *mut mmap::Map) -> ();
-    pub fn SSC_MemMap_sync(map: *const mmap::Map) -> c::Error;
-    pub fn SSC_MemMap_del(map: *mut mmap::Map) -> ();
+    pub fn SSC_MemMap_unmap(map: *mut Map) -> ();
+    pub fn SSC_MemMap_sync(map: *const Map) -> c::Error;
+    pub fn SSC_MemMap_del(map: *mut Map) -> ();
 }
