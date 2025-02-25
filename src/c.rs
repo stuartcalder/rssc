84a6faad471e137988e5b7d3595d711c6e2a91ca
Stuart Calder <technoglub@protonmail.com>
2025-02-24 21:34:26 -0800

    License this code under GPLv3.

--- src/c.rs
+++ src/c.rs
@@ -1,3 +1,20 @@
+/* *
+ * rssc - Wrap the C library SSC in a Rust wrapper. (https://github.com/stuartcalder/SSC)
+ * Copyright (C) 2025 Stuart Calder
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use cty::{c_uint,uint8_t,uint16_t,uint32_t,uint64_t};
 use cty::{c_int,int32_t,int64_t};
 pub type Error      = cty::c_int;
